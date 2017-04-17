4657a847a62f5fbb2b446b321e59acdd3d35216d
pythonix <pythonix@yahoo.com>
2017-04-17 10:14:50 -0400

    added output values for public DNS names

---
+++ tf/create-ecs-cluster/outputs.tf
@@ -0,0 +1,11 @@
+output "jira_url" {
+	value = "${module.atb-jira.public_dns}"
+}
+
+output "confluence_url" {
+	value = "${module.atb-confluence.public_dns}"
+}
+
+output "bamboo_url" {
+	value = "${module.atb-bamboo.public_dns}"
+}
